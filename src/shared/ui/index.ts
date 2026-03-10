6b23f93f8d8865ab40a5558e45bff10c6474ae4b
Nadiia Savchuk <nadiia.savchuk.w@gmail.com>
2026-03-10 14:56:06 +0200

    feat(home): add HomeHero section (#7)

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,4 @@
 export { default as Button } from './Button/Button.tsx';
-export { default as Header } from './Header/Header.tsx';
 export { default as Icon } from './Icon/Icon.tsx';
 export { default as MainLayout } from './layout/MainLayout/MainLayout.tsx';
 export { default as Loader } from './Loader/Loader.tsx';
